db1e2cb9eadccf7e96e896066d2695a64bd41be9
Mooliner <gerymoliner@gmail.com>
2022-05-24 17:07:10 +0200

    Initial commit

---
+++ app/src/main/java/com/moliner/projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner/EsculturaAdapterFirestore.java
@@ -0,0 +1,52 @@
+package com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class EsculturaAdapterFirestore extends FirestoreRecyclerAdapter<com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura, EsculturaAdapterFirestore.EsculturaHolder> {
+
+    public EsculturaAdapterFirestore(@NonNull FirestoreRecyclerOptions<com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura> options) {
+        super(options);
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull EsculturaHolder holder, int position, @NonNull com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura model) {
+        holder.tvNomEscultura.setText(model.getNom().get("ca"));
+        //holder.ivEscultura.setImageResource(R.drawable.foto2);
+        Bitmap bmp = BitmapFactory.decodeByteArray(model.getImatges().get(0).toBytes(), 0, model.getImatges().get(0).toBytes().length);
+        holder.ivEscultura.setImageBitmap(
+                Bitmap.createScaledBitmap(
+                        bmp, 400, 400, true));
+    }
+
+    @NonNull
+    @Override
+    public EsculturaHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.escultures, parent, false);
+        return new EsculturaHolder(view);
+    }
+
+    public class EsculturaHolder extends RecyclerView.ViewHolder {
+        ImageView ivEscultura;
+        TextView tvNomEscultura;
+
+        public EsculturaHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivEscultura = itemView.findViewById(R.id.ivEscultura);
+            tvNomEscultura = itemView.findViewById(R.id.tvNomEscultura);
+        }
+    }
+}
---
+++ app/src/main/java/com/moliner/projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner/EsculturesFragment.java
@@ -0,0 +1,85 @@
+package com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class EsculturesFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    private Context context;
+    RecyclerView rvEscultures;
+    FirebaseFirestore db;
+    com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.EsculturaAdapterFirestore adapter;
+
+    public EsculturesFragment() {
+
+    }
+
+
+    public static EsculturesFragment newInstance(String param1, String param2) {
+        EsculturesFragment fragment = new EsculturesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_escultures, container, false);
+        db = FirebaseFirestore.getInstance();
+        rvEscultures = (RecyclerView)view.findViewById(R.id.rvEscultures);
+        rvEscultures.setHasFixedSize(false);
+        rvEscultures.setLayoutManager(new GridLayoutManager(context, 2));
+        Query consulta = db.collection("Escultures").limit(50);
+        FirestoreRecyclerOptions<com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura> opcions =
+                new FirestoreRecyclerOptions
+                        .Builder<com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura>()
+                        .setQuery(consulta, com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.Escultura.class)
+                        .build();
+
+        adapter = new com.moliner.projectem7appmonistrolericbenitogerardgimemzmarczuritagerardmoliner.EsculturaAdapterFirestore(opcions);
+        rvEscultures.setAdapter(adapter);
+
+        return view;
+    }
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
